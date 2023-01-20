e879536c6721ec789da0514a6c232570ffd7d785
Teklu Mezgebo <mezge002@umn.edu>
2023-01-20 14:40:31 -0600

    commit tree 22e82080da4a1e921f05b4b2332282addded3778

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2 
+
+const random = Math.ceil(Math.random())
+
+const num3 = 40
+const num4 = 6
+
+const mod = num3 % num4
+
+const max = Math.max(1,2,3,20)
